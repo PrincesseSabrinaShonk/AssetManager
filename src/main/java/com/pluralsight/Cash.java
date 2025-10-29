0a3b38deb6ac9cc38e968b983a45cd72d8abcbbc
Sabrina D. <sabrinadorzin103@gmail.com>
2025-10-29 15:44:59 -0400

    Initial Files

--- src/main/java/com/pluralsight/Cash.java
+++ src/main/java/com/pluralsight/Cash.java
@@ -1,4 +1,10 @@
 package com.pluralsight;
 
-public class Cash {
+public class Cash extends Asset{
+    public Cash(String description, String dateAcquired, double originalCost) {
+        super(description, dateAcquired, originalCost);
+    }
+    public double getValue(){
+        return  getOriginalCost();
+    }
 }
